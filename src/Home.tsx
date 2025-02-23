9ff2d4cd8d9b1a0a328b005980339599956d86e2
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-23 15:51:41 -0300

    feat: create Home Page layout

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,51 @@
+import { useEffect, useState } from "react"
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
+import getAllTasks from "./db/getAllTasks"
+import createTask from "./db/createTask"
+import deleteTask from "./db/deleteTask"
+import updateTask from "./db/updateTask"
+
+type Task = {
+  id: number,
+  title: string,  
+  description: string,
+  status: string
+}
+
 export default function Home() {
+  const [ tasks, setTasks ] = useState<Task[]>([])
+
+  useEffect(() => {
+    getAllTasks().then(data => setTasks(data))
+  }, [tasks])
+
+  function createNewTask(title: string, description: string) {
+    createTask(title, description)
+    setTasks(tasks)
+  }
+
+  function deleteTaskById(id: number) {
+    deleteTask(id)
+    setTasks(tasks)
+  }
+
+  function changeTaskStatus(id: number) {
+    const task = tasks.find(task => task.id === id)
+    if (task?.status === 'pending') {
+      updateTask(id, 'completed')
+    } else {
+      updateTask(id, 'pending')
+    }
+  }
+
   return (
-    <h1 className="text-3xl">This is the Home Page</h1>
+    <div className="h-dvh bg-[#000117] flex flex-col items-center py-8 gap-8">
+      <div className="h-dvh bg-[#000117] flex flex-col items-center py-8 gap-8 w-96 md:w-[566px]">
+        <h1 className="text-white font-semibold text-4xl">Lista de Tarefas</h1>
+        <AddTask createNewTask={createNewTask} />
+        <Tasks tasks={tasks} deleteTaskById={deleteTaskById} changeTaskStatus={changeTaskStatus} />
+      </div>
+    </div>
   )
 }
