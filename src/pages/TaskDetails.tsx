3698b912d0dbf848ff2e89d41f44c7a542fc2ad4
jpchavescruz <j.pvplayer0@gmail.com>
2025-02-23 17:14:59 -0300

    feat: get task details by route params

--- src/pages/TaskDetails.tsx
+++ src/pages/TaskDetails.tsx
@@ -1,17 +1,20 @@
 import { ChevronLeft } from "lucide-react"
+import { useSearchParams } from "react-router"
 
 export default function TaskDetails() {
+  const [ searchParams ] = useSearchParams()
+
   return (
     <div className="h-dvh bg-[#000117] flex flex-col items-center py-8 gap-8">
       <div className="h-dvh bg-[#000117] flex flex-col items-center py-8 gap-8 w-96 md:w-[566px]">
-        <h1 className="text-white font-semibold text-4xl">Descrição da tarefa: Limpar o PC</h1>
+        <h1 className="text-white text-center font-semibold text-4xl">Descrição da tarefa: {searchParams.get('title')}</h1>
         <div className="bg-[#13A8FE] w-full rounded-3xl flex flex-col justify-center items-center gap-7 p-8" >
           <a href="/" className="flex place-self-start items-center">
             <ChevronLeft />
             <p className="text-[#000017] font-semibold text-[20px]">Voltar</p>
           </a>
           <div className="bg-white p-6 w-full rounded-3xl">
-            <p className="text-2xl text-[#000017]">Trocar a pasta térmica e tirar a poeira dos componentes.</p>
+            <p className="text-2xl text-[#000017]">{searchParams.get('description')}</p>
           </div>
         </div>
       </div>
